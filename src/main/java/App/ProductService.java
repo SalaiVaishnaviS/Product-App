809fa6726c4fde61668e90d9c117bef0c382843c
Salai.Vaishnavi <Salai.Vaishnavi@truminds.com>
2022-07-26 21:08:38 +0530

    Initial commit

---
+++ src/main/java/App/ProductService.java
@@ -0,0 +1,20 @@
+package App;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll() {
+        return repo.findAll();
+    }
+     
+    public void save(Product product) {
+        repo.save(product);
+    }
+}
